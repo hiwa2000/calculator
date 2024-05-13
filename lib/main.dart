37b0af81a6f6f13335d8bb02abdfca02b5e53755
hiwa Taheri <hiwataheri@hiwas-MBP.fritz.box>
2024-05-13 14:53:13 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowMaterialGrid: false,
+      debugShowCheckedModeBanner: false,
       home: CalculatorScreen(),
     );
   }
